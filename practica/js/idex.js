296e5a002438cd954bf04bf8cabd1858f4c59a63
Alexiaalurralde <aalurralde@udesa.edu.ar>
2020-10-13 16:07:50 -0300

    practica

--- practica/js/idex.js
+++ practica/js/idex.js
@@ -1,31 +1,38 @@
-//1 
-
+//1
 alert ("Bienvenidos a mi sitio!");
 
-//2 a,b
-
-
-let avanzar = confirm ("¿Estas seguro de querer avanzar?");
+//2
+let avanzar = confirm ("¿Esta seguro que desea continuar?");
+terminar = document.querySelector (".terminar");
 
 if (avanzar == false) {
-    let avanzarFalse = document.querySelector (".nosigue");
-    avanzarFalse.innerHTML = "Lamentamos que no quieras continuar tu visita por este sitio increíble";
+    terminar.innerHTML = "Lamentamos que no quieras continuar tu visita por este sitio increible"
 }
-
 else if (avanzar == true) {
-    let avanzarTrue = document.querySelector (".nosigue");
-    avanzarTrue.innerHTML = "¡Qué alegría que quieras seguir con tu visita por este maravilloso sitio!";
+    terminar.innerHTML = "¡Qué alegría que quieras seguir con tu visita por este maravilloso sitio!";
+
+let name = prompt ("Ingrese su nombre");  
+saludo = document.querySelector(".usuario");
+saludo.innerHTML = "Bienvendido " + name;
+
+edad = prompt ("¿Cual es su edad?");
+if (edad > 18) {
+    alert("Puede continuar con el proceso");
+    oscuro = confirm ("¿Desea utilizar el modo oscuro?");
+    body = document.querySelector(".body");
 }
-
-
-
-//2 c
-
-
-let nombre = prompt ("Ingrese su nombre");
-
-let bienvenido = document.querySelector ("usuario");
-bienvenido.innerHTML = "Bienvenido" + nombre;
-
-
+else if (edad < 18) {
+    alert("Acceso denegado");
+}
+if (oscuro == true) {
+    body.style.backgroundColor = "dimgray"; 
+    body.style.color = "white";
+}
+ 
+//3
+let diaDeSemana = "Domingo";
+if (diaDeSemana = "Domingo") {
+    alert ("¡Hoy es día de descanso!")
+}
+}
 
